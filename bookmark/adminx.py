f53a42f8a244527e83d82d73f1d1dfc8716ccdc3
aliyun <toozoofoo@gmail.com>
2013-11-05 21:03:12 +0800

    add user to Bookmark unicode representation

--- bookmark/adminx.py
+++ bookmark/adminx.py
@@ -8,8 +8,10 @@ from xadmin.plugins.batch import BatchChangeAction
 
 from bookmark.models import Bookmark, List, Feedback
 from eventlog.models import Log
+from notifications.models import Notification
 
 xadmin.site.register(Bookmark)
 xadmin.site.register(List)
 xadmin.site.register(Log)
 xadmin.site.register(Feedback)
+xadmin.site.register(Notification)

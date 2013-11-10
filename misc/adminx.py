8a46b4cf023486fdb829c30c2f151b95a5575871
aliyun <toozoofoo@gmail.com>
2013-11-10 21:17:19 +0800

    unicode function for Lock

---
+++ misc/adminx.py
@@ -0,0 +1,12 @@
+import xadmin
+from xadmin import views
+from models import *
+from xadmin.layout import *
+
+from xadmin.plugins.inline import Inline
+from xadmin.plugins.batch import BatchChangeAction
+
+from misc.models import Lock
+
+xadmin.site.register(Lock)
+
--- misc/models.py
+++ misc/models.py
@@ -8,3 +8,6 @@ class Lock(models.Model):
     action = models.CharField(max_length=32)
     user = models.ForeignKey(User)
     created_time = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "{} - {}".format(self.user, self.action)

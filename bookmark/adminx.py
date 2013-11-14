1f8f2e60ce7ae217bc5fff013307b8a45e0cb59d
fay <toozoofoo@gmail.com>
2013-11-14 22:30:54 +0800

    add UserSocialAuth in adminx

--- bookmark/adminx.py
+++ bookmark/adminx.py
@@ -9,9 +9,11 @@ from xadmin.plugins.batch import BatchChangeAction
 from bookmark.models import Bookmark, List, Feedback
 from eventlog.models import Log
 from notifications.models import Notification
+from social_auth.models import UserSocialAuth
 
 xadmin.site.register(Bookmark)
 xadmin.site.register(List)
 xadmin.site.register(Log)
 xadmin.site.register(Feedback)
 xadmin.site.register(Notification)
+xadmin.site.register(UserSocialAuth)

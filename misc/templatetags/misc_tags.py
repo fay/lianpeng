f3692f328119c22e9cc17a038145250eaefaf954
fay <toozoofoo@gmail.com>
2014-04-17 12:28:05 +0800

    add total bookmarks in the homepage

--- misc/templatetags/misc_tags.py
+++ misc/templatetags/misc_tags.py
@@ -1,5 +1,7 @@
 from django.template.defaultfilters import register
 from django.conf import settings
+from django.core.cache import cache
+from bookmark.models import Bookmark
 
 def grouped(l, n): 
     # Yield successive n-sized chunks from l.
@@ -9,4 +11,12 @@ def grouped(l, n):
 @register.filter
 def group_by(value, arg):
     return grouped(value, arg)
+
+@register.simple_tag()
+def total_bookmarks():
+    total = cache.get('lp_total_bookmarks')
+    if not total:
+        total = Bookmark.objects.all().count()
+        cache.set('lp_total_bookmarks', total, 60 * 60 * 1)
+    return total
 

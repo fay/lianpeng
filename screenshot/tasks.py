9a9ac8c57ee55a739da9c7e3400887af0c555f3f
aliyun <toozoofoo@gmail.com>
2014-05-13 22:56:15 +0800

    fix issues

--- screenshot/tasks.py
+++ screenshot/tasks.py
@@ -9,7 +9,7 @@ from screenshot.models import Screenshot
 def create_screenshot_task(bookmark):
     url = bookmark.url
     image = "{}/screenshots/{}.png".format(settings.MEDIA_ROOT, bookmark.unique_key)
-    image_url = "{}/screenshots/{}.png".format(settings.MEDIA_URL, bookmark.unique_key)
+    image_url = "{}screenshots/{}.png".format(settings.MEDIA_URL, bookmark.unique_key)
     os.system("wkhtmltoimage --height 786 --quality 80 {} {}".format(url, image))
     screenshot = Screenshot(bookmark=bookmark)
     screenshot.image.name = image_url

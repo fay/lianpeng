e76ae9dd273da7dc51572cc961e210f36f02de09
aliyun <toozoofoo@gmail.com>
2014-06-17 21:47:56 +0800

    change favicon url

--- bookmark/models.py
+++ bookmark/models.py
@@ -122,7 +122,8 @@ class Bookmark(models.Model, DiffingMixin):
 
     @property
     def favicon(self):
-        return 'http://g.etfv.co/http://{}?defaulticon=lightpng'.format(self.domain)
+        return 'https://icons.duckduckgo.com/ip2/{}.ico'.format(self.domain)
+        #return 'http://g.etfv.co/http://{}?defaulticon=lightpng'.format(self.domain)
         #return 'https://s2.googleusercontent.com/s2/favicons?alt=site&domain={}'.format(self.domain)
 
     def absolute_url(self):
--- service/tasks.py
+++ service/tasks.py
@@ -1,4 +1,5 @@
 import urllib2
+import urllib
 
 from celery.task import task
 from url2feed import extract
@@ -7,6 +8,7 @@ from url2feed import extract
 
 @task
 def get_feed_url(url, callback):
+    print url, callback
     result = extract(url)
-    urllib2.urlopen(callback, result)
+    resp = urllib2.urlopen(callback, urllib.urlencode(result))
     return result
